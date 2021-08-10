3d8331225c69493f128a5d00e8b284f985520faa
Tyra Carter <84162602+teecarter16@users.noreply.github.com>
2021-08-09 20:43:02 -0700

    header component for skate shop

--- SkateShop/skate-store/src/Header.js
+++ SkateShop/skate-store/src/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Header.css';
+import swbshop from './swbshop.JPG';
 
 function Header() {
     return (
         <div className="header">
             <img 
-                src="../images/skatelogo.PNG"
+                src={swbshop}
                 alt="SKate shop logo" 
                 className="header_logo" />
+
+                <div className="header_search">
+                    <input className="headersearchIn"></input>
+                </div>
+
+                <div className="header_nav"></div>
         </div>
+        
     )
 }
 
