5217165bd6c2f89cef4d5bad107987b7b240b3f5
Tyra Carter <84162602+teecarter16@users.noreply.github.com>
2021-08-10 09:39:23 -0700

    finally got the material ui to work

--- SkateShop/skate-store/src/App.js
+++ SkateShop/skate-store/src/App.js
@@ -5,7 +5,6 @@ import Header from './Header'
 function App() {
   return (
     <div className="App">
-     <h1>Lets build a skate shop !!</h1>
 
     <Header />
     {/** Home*/}
--- SkateShop/skate-store/src/Header.js
+++ SkateShop/skate-store/src/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import './Header.css';
 import swbshop from './swbshop.JPG';
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
     return (
         <div className="header">
+            
             <img 
-                src={swbshop}
-                alt="SKate shop logo" 
-                className="header_logo" />
+                 src={swbshop}
+                 alt="SKate shop logo" 
+                 className="header_logo" />
 
-                <div className="header_search">
-                    <input className="headersearchIn"></input>
+            <div className="header_search">
+
+                    <input 
+                        className="header_searchInput"
+                        type="text"></input>
+                   <SearchIcon className="header_searchIcon" /> 
+
                 </div>
 
-                <div className="header_nav"></div>
+                <div className="header_nav">
+                    <div className="header_option">
+                        <span className="header_optionLineOne">Hello,Skater</span>
+                        <span className="header_optionLineTwo">Sign in</span>
+                    </div>
+
+                    <div className="header_option">
+                        <span className="header_optionLineOne">Returns</span>
+                        <span className="header_optionLineTwo">Orders</span>
+                    </div>
+
+                    <div className="header_option">
+                        <span className="header_optionLineOne">your</span>
+                        <span className="header_optionLineTwo">SWB Bag</span>
+                    </div>
+                </div>
         </div>
         
     )
